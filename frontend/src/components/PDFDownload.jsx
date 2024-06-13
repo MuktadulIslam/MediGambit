93b9d781d943fce03fcab4dd604bd57e72464a5b
Muktadul Islam <76801029+MuktadulIslam@users.noreply.github.com>
2024-06-13 11:25:56 +0600

    solve pdf downloader error

--- frontend/src/components/PDFDownload.jsx
+++ frontend/src/components/PDFDownload.jsx
@@ -13,31 +13,30 @@ export default function PDFDownload({ questions, examdetails }) {
     useEffect(() => {
         if (firstTimeMount.current) {
             window.print();
-            setTimeout(router.back(),1500)
             firstTimeMount.current = false;
         }
     }, []);
 
     return (<>
         <div className="bg-white">
-            <div className='absolute top-0 z-50 w-full h-auto border-b-2 border-black bg-white'>
-                <div className="h-36 md:h-40  flex justify-center items-center py-5">
-                    <img src="/imgs/icons/medigambit.png" alt="" className="h-full object-cover" />
+            <div className='absolute top-0 z-50 w-full h-auto bg-white border-b-2 border-black'>
+                <div className="flex items-center justify-center py-5 h-36 md:h-40">
+                    <img src="/imgs/icons/medigambit.png" alt="" className="object-cover h-full" />
                 </div>
-                <div className="h-6 w-full flex justify-center">
-                    <h3 className="h-full px-6 text-lg font-bold flex items-center rounded-full">{examtopic}</h3>
+                <div className="flex justify-center w-full h-6">
+                    <h3 className="flex items-center h-full px-6 text-lg font-bold rounded-full">{examtopic}</h3>
                 </div>
-                <div className="h-5 w-full flex justify-between px-1 md:px-2">
+                <div className="flex justify-between w-full h-5 px-1 md:px-2">
                     <p className="text-sm">{examsubject}</p>
                     <p className="text-sm">{examname}</p>
                 </div>
             </div>
 
-            <div className="w-full px-1 sm:px-2 md:px-4 pt-6">
-                <div className="columns-2 gap-3 pb-14 space-y-2">
+            <div className="w-full px-1 pt-6 sm:px-2 md:px-4">
+                <div className="gap-3 space-y-2 columns-2 pb-14">
                     {
                         questions.map((question, index) =>
-                            <div key={index} className="card-white text-sm relative">
+                            <div key={index} className="relative text-sm card-white">
                                 <div className="flex items-start gap-2 mb-2">
                                     <span>{index + 1}.</span>
                                     <div dangerouslySetInnerHTML={{ __html: question.question }}></div>
@@ -47,22 +46,22 @@ export default function PDFDownload({ questions, examdetails }) {
                                     {
                                         question.options.map((o, oIndex) =>
                                             <div key={oIndex} className="flex items-start gap-2">
-                                                <div className="w-5 h-5 flex justify-center items-center bg-white border shadow-md rounded-full">{optionIndex[oIndex]}</div>
+                                                <div className="flex items-center justify-center w-5 h-5 bg-white border rounded-full shadow-md">{optionIndex[oIndex]}</div>
                                                 <div dangerouslySetInnerHTML={{ __html: o }}></div>
                                             </div>)
                                     }
                                 </div>
-                                <img src="/imgs/icons/medigambit.png" className="w-12 auto absolute right-2 bottom-1" />
+                                <img src="/imgs/icons/medigambit.png" className="absolute w-12 auto right-2 bottom-1" />
                             </div>)
                     }
                 </div>
             </div>
 
             <div className="w-full h-auto">
-                <div className='h-7 w-full text-center text-lg font-semibold text-gray-600'>উত্তরপত্র</div>
-                <div className="columns-2 px-1 sm:px-2 md:px-4 ">
+                <div className='w-full text-lg font-semibold text-center text-gray-600 h-7'>উত্তরপত্র</div>
+                <div className="px-1 columns-2 sm:px-2 md:px-4 ">
                     {questions.map((question, q_index) =>
-                        <div key={q_index} className="h-7 card-white text-sm relative flex items-center">
+                        <div key={q_index} className="relative flex items-center text-sm h-7 card-white">
                             <div className='w-6 text-base'>{q_index + 1}. </div>
                             <div className="flex gap-2">
                                 {question.options.map((option, index) => (
